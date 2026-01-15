467f8cd011483400a2268c0ca5fa45ffd1707ac9
Page Chen <pagechen04@gmail.com>
2026-01-15 20:04:08 +0800

    fix: remove pprof

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"net/http"
-	_ "net/http/pprof"
-
 	log "github.com/sirupsen/logrus"
 
 	"github.com/XrayR-project/XrayR/cmd"
 )
 
 func main() {
-	go func() {
-		http.ListenAndServe("[::]:6060", nil)
-	}()
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
